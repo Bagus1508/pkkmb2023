bba040dee0c617f78f9e9f991b821ebdd947163a
Bagus Adianto <bagusadianto5@gmail.com>
2023-08-07 23:40:59 +0700

    Fix : View Presensi

--- resources/views/dashboard/admin/presences/show.blade.php
+++ resources/views/dashboard/admin/presences/show.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="mb-5">
-    <div class="w-1/2 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+    <div class="w-full lg:w-1/2 p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8">
         <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{{ $attendance->title }}</h5>
         <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">{{ $attendance->description }}</p>
         <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 my-2">
@@ -26,13 +26,9 @@
         </div>
 
         <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 my-2">
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-x-4">
+            <div class="flex gap-x-4">
                 <div class="mb-2">
                     <small class="font-semibold bg-gray-100 rounded-lg px-2 py-1">Range Jam Masuk</small>
-                    <div class="mt-1">{{ $attendance->start_time }} - {{ $attendance->batas_start_time }}</div>
-                </div>
-                <div class="mb-2">
-                    <small class="font-semibold bg-gray-100 rounded-lg px-2 py-1">Range Jam Keluar</small>
                     <div class="mt-1">{{ $attendance->start_time }} - {{ $attendance->batas_start_time }}</div>
                 </div>
             </div>
