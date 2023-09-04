1f3319ce86c0e33dd273385184fe98d9b5de8d1e
Ardhya Pandu <ardhya.pandu@yahoo.com>
2023-09-04 12:53:40 +0700

    feat: Update Halaman Utaman Mobile

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -20,11 +20,11 @@
     <div class="relative">
       @include('includes.landing.navbarlanding')
 
-      <section id="hero" class="animation-container h-screen w-full hero-background relative flex items-center">
+      <section id="hero" class="lg:h-screen w-full hero-background lg:relative flex items-center">
         <img 
           src="{{ asset('/src/img/hero/bintang.png') }}" 
           alt="bintang" 
-          class="animate__animated animate__fadeInUp absolute mt-0 ml-0 h-screen w-full object-cover"
+          class="hidden lg:block absolute mt-0 ml-0 h-screen w-full object-cover"
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-back"
           data-aos-delay="1000"
@@ -32,11 +32,11 @@
           data-aos-offset="0"
         >
     
-        <div class="absolute w-full h-screen flex flex-col items-center text-center pt-36">
+        <div class="lg:absolute w-full h-screen flex flex-col items-center text-center pt-72 lg:pt-36">
           <img 
             src="{{ asset('/src/img/logo/logo.png') }}" 
             alt="" 
-            class="h-14 bg-white py-1 px-6 rounded-lg mb-5"
+            class="h-9 lg:h-14 bg-white py-1 px-3 lg:px-6 rounded-lg mb-5"
             data-aos="fade-down"
             data-aos-easing="linear"
             data-aos-delay="1500"
@@ -50,8 +50,8 @@
             data-aos-delay="1000"
             data-aos-duration="1000"
           >
-            <h2 id="title1" class="text-space-textungu text-6xl font-extrabold mb-0">WUJUDKAN MIMPI</h2>
-            <h2 id="title2" class="text-white text-6xl font-extrabold mb-5">TAKLUKAN DUNIA</h2>
+            <h2 id="title1" class="text-space-textungu text-3xl lg:text-6xl font-extrabold mb-0">WUJUDKAN MIMPI</h2>
+            <h2 id="title2" class="text-white text-3xl lg:text-6xl font-extrabold mb-5">TAKLUKAN DUNIA</h2>
           </div>
 
           <div
@@ -60,27 +60,27 @@
             data-aos-easing="linear"
             data-aos-duration="2500"
           >
-            <h3 id="text1" class="text-white text-lg font-light">Inilah saatnya untuk beraksi</h3>
-            <h3 id="text2" class="text-white text-lg font-light">Jadilah bagian dari perubahan yang luar biasa</h3>
+            <h3 id="text1" class="text-white text-sm lg:text-lg font-light">Inilah saatnya untuk beraksi</h3>
+            <h3 id="text2" class="text-white text-sm lg:text-lg font-light">Jadilah bagian dari perubahan yang luar biasa</h3>
           </div>
         </div>
         
         <img 
           src="{{ asset('/src/img/hero/planet.png') }}" 
           alt="planet" 
-          class="absolute mt-0 ml-0 h-screen w-full object-cover"
+          class="hidden lg:block absolute mt-0 ml-0 h-screen w-full object-cover"
           data-aos="fade-up"
         >
         <img
           src="{{ asset('/src/img/hero/planett.png') }}"
           alt="planett"
-          class="absolute mt-0 ml-0 h-screen w-full object-cover"
+          class="hidden lg:block absolute mt-0 ml-0 h-screen w-full object-cover"
           data-aos="zoom-in"
           data-aos-delay="500"
         >
     
         <div 
-          class="absolute w-full h-screen flex flex-col items-center text-center pt-96"
+          class="absolute w-full h-screen flex flex-col items-center text-center pt-80"
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-back"
           data-aos-delay="2000"
@@ -88,7 +88,7 @@
           data-aos-offset="0"
         >
           <a href="#dashboard">
-            <button class="text-white font-bold text-xl mt-28 py-2 px-7 bg-space-buttonungu rounded-full shadow-sm">
+            <button class="text-white font-bold text-base lg:text-xl mt-52 lg:mt-44 py-1 lg:py-2 px-6 lg:px-7 bg-space-buttonungu rounded-full shadow-sm">
               Mulai
             </button>
           </a>
@@ -98,7 +98,8 @@
       <section id="dashboard" class="h-screen w-full hero-section2-background">
         <img 
           src="{{ asset('/src/img/hero/2/bintang.png') }}" 
-          alt="bintang" class="absolute mt-0 ml-0 h-screen object-cover"
+          alt="bintang"
+          class="absolute mt-0 ml-0 h-screen object-cover"
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-back"
           data-aos-duration="2000"
@@ -123,11 +124,11 @@
           data-aos-duration="1000"
         >
     
-        <div class="absolute ml-36 h-screen">
-          <div class="flex flex-col items-center text-center pt-48">
+        <div class="lg:absolute w-full h-screen">
+          <div class="flex flex-col items-center text-center pt-80 lg:pt-36">
             <h2 
               id="title1" 
-              class="text-white text-6xl font-extrabold mb-1"
+              class="text-white text-3xl lg:text-6xl font-extrabold mb-0"
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-delay="1100"
@@ -137,7 +138,7 @@
             </h2>
             <h2 
               id="title2" 
-              class="text-white text-3xl font-bold mb-7"
+              class="text-white text-lg lg:text-3xl font-bold mb-5"
               data-aos="fade-down"
               data-aos-easing="linear"
               data-aos-delay="1000"
@@ -152,8 +153,8 @@
               data-aos-delay="1300"
               data-aos-duration="1000"
             >
-              <h3 id="text1" class="text-white text-base font-light">Saatnya mulai beraksi</h3>
-              <h3 id="text2" class="text-white text-base font-light mb-7">Buat pengalamanmu menjadi lebih berharga</h3>
+              <h3 id="text1" class="text-white text-sm lg:text-base font-light">Saatnya mulai beraksi</h3>
+              <h3 id="text2" class="text-white text-sm lg:text-base font-light mb-7">Buat pengalamanmu menjadi lebih berharga</h3>
             </div>
     
             <a 
@@ -163,7 +164,7 @@
               data-aos-delay="2500"
               data-aos-duration="1000"
             >
-              <button class="text-white font-bold text-base py-2 px-9 bg-space-textungu border-2 border-white rounded-full shadow-xl">
+              <button class="text-white font-bold text-base py-1 lg:py-2 px-6 lg:px-9 bg-space-textungu border-2 border-white rounded-full shadow-xl">
                 Masuk
               </button>
             </a>
@@ -193,11 +194,11 @@
           <img src="{{ asset('/src/img/hero/3/roket.png') }}" alt="roket" class="absolute mt-0 ml-0 h-screen object-cover">
         </div>
     
-        <div class="absolute ml-36 h-screen">
-          <div class="flex flex-col mt-48">
+        <div class="absolute ml-5 lg:ml-36 h-screen">
+          <div class="flex flex-col mt-64 lg:mt-48">
             <h2 
               id="title1" 
-              class="text-orange-500 text-5xl font-extrabold mb-1"
+              class="text-orange-500 text-3xl lg:text-5xl font-extrabold mb-0 lg:mb-1"
               data-aos="fade-right"
               data-aos-easing="linear"
               data-aos-delay="2100"
@@ -207,7 +208,7 @@
             </h2>
             <h2 
               id="title2" 
-              class="text-orange-500 text-3xl font-bold mb-7"
+              class="text-orange-500 text-xl lg:text-3xl font-bold mb-5 lg:mb-7"
               data-aos="fade-right"
               data-aos-easing="linear"
               data-aos-delay="2000"
@@ -216,14 +217,14 @@
               MEMBUATMU MENJADI LEBIH LUAS
             </h2>
             <div
-              class=""
+              class="max-w-screen-sm lg:max-w-screen-xl"
               data-aos="fade-right"
               data-aos-easing="linear"
               data-aos-delay="2300"
               data-aos-duration="1000"
             >
-              <h3 id="text1" class="text-space-buttonungu text-lg font-light">Informasi akan membantumu</h3>
-              <h3 id="text2" class="text-space-buttonungu text-lg font-light mb-7">Informasi akan menjadi penuntunmu</h3>
+              <h3 id="text1" class="text-space-buttonungu text-base lg:text-lg font-light">Informasi akan membantumu</h3>
+              <h3 id="text2" class="text-space-buttonungu text-base lg:text-lg font-light mb-5 lg:mb-7">Informasi akan menjadi penuntunmu</h3>
             </div>
     
             <a 
@@ -233,7 +234,7 @@
               data-aos-delay="2500"
               data-aos-duration="1000"
             >
-              <button class="text-white font-bold text-lg py-2 px-9 bg-space-buttonungu border-2 border-white rounded-full shadow-xl">
+              <button class="text-white font-bold text-base lg:text-lg py-1 lg:py-2 px-5 lg:px-9 bg-space-buttonungu border-2 border-white rounded-full shadow-xl">
                 Cari Informasi
               </button>
             </a>
@@ -241,9 +242,9 @@
         </div>
       </section>
     
-      <section id="news" class="">
+      {{-- <section id="news" class="">
     
-      </section>
+      </section> --}}
 
       @include('layouts.landing.footer')
 
