ba058c6034c98d013e41af0f3d408cfb917a97f3
Bagus Adianto <bagusadianto5@gmail.com>
2023-09-15 10:44:58 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TambahTugasController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\NewsController;
-<<<<<<< HEAD
 use App\Http\Controllers\ResultTaskController;
-=======
 use App\Http\Controllers\JenisKetentuanController;
 use App\Http\Controllers\KetentuanController;
-
->>>>>>> f857bc48e69fa21e1daebcc6f120dbeb558a93ac
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
