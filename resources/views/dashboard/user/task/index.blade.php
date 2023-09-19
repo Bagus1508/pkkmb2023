526469192db79c8e697dbce36e7d475a45f1362b
Bagus Adianto <bagusadianto5@gmail.com>
2023-09-19 13:31:27 +0700

    Update : Fix Waktu Mulai Tugas tombol "Lihat"

--- resources/views/dashboard/user/task/index.blade.php
+++ resources/views/dashboard/user/task/index.blade.php
@@ -89,7 +89,7 @@
                         {{ $item->batas_start_time }}
                     </td>
                     <td class="px-6 py-4">
-                        <div class="flex items-center">
+                        <div class="flex items-center">     
                              @include('partials.task-badges')
                         </div>
                     </td>
@@ -120,7 +120,47 @@
                         @endif
                     </td>                             
                     <td class="px-6 py-4">
+                    @if ($item->data->is_start)
                         <a href="{{ route('dashboard-user.taskshow', $item->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Lihat</a>
+                    @else
+                        
+                    <!-- Modal toggle -->
+                    <button data-modal-target="staticModal" data-modal-toggle="staticModal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" type="button">
+                        Lihat
+                    </button>
+
+                    <!-- Main modal -->
+                    <div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative w-full max-w-2xl max-h-full">
+                        <!-- Modal content -->
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <!-- Modal header -->
+                            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Informasi Tugas
+                                </h3>
+                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="staticModal">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="p-6 space-y-6">
+                                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    Maaf, untuk pengumpulan tugas <span class="font-bold">"{{ $item->title }}"</span> ini sedang ditutup.
+                                </p>
+                            </div>
+                            <!-- Modal footer -->
+                            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                <button data-modal-hide="staticModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Tutup</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+  
+                    @endif
                     </td>
                 </tr>
                 @endforeach
